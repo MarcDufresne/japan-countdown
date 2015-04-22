c018fccb8c3005a38a65d7a03904119d87a738f4
MarcDufresne <marc.andre.dufresne@gmail.com>
2015-04-22 19:41:53 -0400

    Added JS refresh

--- japan.py
+++ japan.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+import json
+from flask.wrappers import Response
 import pytz
 
 from flask import Flask
@@ -14,6 +16,9 @@ def hello_world():
 
     days_left = datetime(year=2015, month=5, day=24) - datetime.today()
 
+    hours_left = datetime(year=2015, month=5, day=24, hour=11,
+                          tzinfo=pytz.timezone('Canada/Eastern')) - datetime.now(tz=pytz.timezone('Canada/Eastern'))
+
     japan_time = datetime.now(tz=pytz.timezone('Asia/Tokyo')).strftime('%B %-d, %H:%M')
 
     weather = {
@@ -25,10 +30,34 @@ def hello_world():
         'is_japan': is_japan,
         'weather': weather,
         'days_left': days_left.days,
+        'hours_left': hours_left.seconds // 3600,
         'japan_time': japan_time
     }
 
     return render_template('main.html', context=ctx)
+
+
+@app.route('/api/time')
+def get_japan_time():
+
+    japan_time = {
+        'time': datetime.now(tz=pytz.timezone('Asia/Tokyo')).strftime('%B %-d, %H:%M')
+    }
+
+    return Response(json.dumps(japan_time), content_type='text/json')
+
+
+@app.route('/api/hours_left')
+def get_hours_left():
+
+    hours_left = datetime(year=2015, month=5, day=24, hour=11,
+                          tzinfo=pytz.timezone('Canada/Eastern')) - datetime.now(tz=pytz.timezone('Canada/Eastern'))
+
+    japan_time = {
+        'hours_left': hours_left.seconds // 3600,
+    }
+
+    return Response(json.dumps(japan_time), content_type='text/json')
 
 
 if __name__ == '__main__':
